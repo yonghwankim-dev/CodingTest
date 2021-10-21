5a0481de21d9a1dcbf00cc3687576b4323458263
yonghwankim.dev <yonghwankim.dev@gmail.com>
2021-10-21 16:42:26 +0900

    boj prime

---
+++ BOJ_1929/MainTest.java
@@ -0,0 +1,22 @@
+package BOJ_1929;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	
+	@Test
+	void test1() {
+		Main.solution(1, 100);
+	}
+	
+	@Disabled
+	@Test
+	void test2() {
+		Main.solution(1, 1000000);
+	}
+
+}
