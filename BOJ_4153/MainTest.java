c312ec00a2653d449327d1f029e1a5b9bd3d2539
yonghwankim.dev <yonghwankim.dev@gmail.com>
2021-10-26 18:46:02 +0900

    boj 4153 right triangle

---
+++ BOJ_4153/MainTest.java
@@ -0,0 +1,28 @@
+package BOJ_4153;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	@Test
+	void test1() {
+		int[] arr = {6,8,10};
+		System.out.println(Main.solution(arr));
+	}
+	
+	@Test
+	void test2() {
+		int[] arr = {25,52,60};
+		System.out.println(Main.solution(arr));
+	}
+
+	@Test
+	void test3() {
+		int[] arr = {5,12,13};
+		System.out.println(Main.solution(arr));
+	}
+	
+	
+}
