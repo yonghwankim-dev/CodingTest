00b7e6d253977dfdc542beb6dba4614aa0b6116c
yonghwankim.dev <yonghwankim.dev@gmail.com>
2021-11-05 17:41:13 +0900

    boj 1436 666

---
+++ BOJ_1436/MainTest.java
@@ -0,0 +1,34 @@
+package BOJ_1436;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	@Test
+	void test0() {
+		System.out.println(Main.solution(1));	
+	}
+	
+	@Test
+	void test1() {
+		System.out.println(Main.solution(2));	
+	}
+	@Test
+	void test2() {
+		System.out.println(Main.solution(3));	
+	}
+	@Test
+	void test3() {
+		System.out.println(Main.solution(6));	
+	}
+	@Test
+	void test4() {
+		System.out.println(Main.solution(187));	
+	}
+	@Test
+	void test5() {
+		System.out.println(Main.solution(500));	
+	}
+}
