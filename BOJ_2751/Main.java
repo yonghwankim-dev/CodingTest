4dbef5e8cc958ed76c7635488792ef6cc062842d
yonghwankim.dev <yonghwankim.dev@gmail.com>
2021-11-10 18:12:07 +0900

    boj 2751

--- BOJ_2751/Main.java
+++ BOJ_2751/Main.java
@@ -11,31 +11,26 @@ import java.util.List;
 
 
 public class Main {
-	public static void main(String args[]) throws IOException
+	
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void solution() throws NumberFormatException, IOException
 	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	    
 		int n = Integer.parseInt(br.readLine());
-		List<Integer> list = new ArrayList<>();
-		
-		
+		List<Integer> list = new ArrayList<Integer>();
+		StringBuilder sb = new StringBuilder();
 		for(int i=0;i<n;i++)
 		{
 			list.add(Integer.parseInt(br.readLine()));
 		}
-		
-		
 		Collections.sort(list);
+		list.stream().forEach(item->{sb.append(item+"\n");});
 		
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            sb.append(list.get(i) + "\n");
-        }
- 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+		System.out.println(sb.toString());
+	}
+	
+	public static void main(String args[]) throws IOException
+	{
+		solution();	
 	}
 }
