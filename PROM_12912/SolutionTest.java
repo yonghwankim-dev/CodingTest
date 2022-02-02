f0e5b547b2a08540226750cf709cc1fa5b5d5326
yonghwan <yonghwankim.dev@gmail.com>
2022-02-02 19:18:37 +0900

    prom 두 정수 사이의 합

---
+++ PROM_12912/SolutionTest.java
@@ -0,0 +1,16 @@
+package PROM_12912;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+	@Test
+	void test() {
+		Solution.solution(-10000000, 10000000);
+	}
+
+}
