3a67a34eb14eedde82640fcace1002f40365dd29
yonghwan <yonghwankim.dev@gmail.com>
2022-02-10 22:54:49 +0900

    boj 1932

--- BOJ_1932/Main.java
+++ BOJ_1932/Main.java
@@ -7,18 +7,6 @@ public class Main {
 
 	static int[][] dp;
 	
-	public static void print(int[][] matrix)
-	{
-		for(int i=0;i<matrix.length;i++)
-		{
-			for(int j=0;j<matrix[i].length;j++)
-			{
-				System.out.print(matrix[i][j] + " ");
-			}
-			System.out.println();
-		}
-	}
-	
 	public static int solution(int[][] matrix, int row, int col, int n)
 	{
 		if(row==n)
