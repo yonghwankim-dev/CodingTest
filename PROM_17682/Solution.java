5d41db0f5992ebdb9ef8998fcf917ec335b42d46
yonghwan <yonghwankim.dev@gmail.com>
2022-02-10 15:48:54 +0900

    prom 17682 dart game

--- PROM_17682/Solution.java
+++ PROM_17682/Solution.java
@@ -1,81 +1,11 @@
 package PROM_17682;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Solution {
-
-	public static String[] splitDartResult(String dartResult)
-	{
-		String regex = "(\\d{1,2}[SDT]+[\\*#]*)";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(dartResult);
-		
-		String[] splitedDartResult = new String[3];
-		int i=0;
-		while(matcher.find())
-		{
-			splitedDartResult[i] = matcher.group();
-			i++;
-		}
-		return splitedDartResult;
-	}
-		
-	public static String[] splitOneResult(String oneResult)
-	{
-		String regex = "(\\d{1,2})([SDT]+)([\\*#])*";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(oneResult);
-		
-		String[] splitedOneReslut = new String[3];
-	
-		int i=1;
-		if(matcher.find())
-		{
-			while(i<=3)
-			{
-				splitedOneReslut[i-1] = matcher.group(i);
-				i++;
-			}
-		}
-		
-		return splitedOneReslut;		
-	}
-	
-	public static int cal(String dartResult, int[] answer, int i)
-	{
-		String[] splitedOneResult = splitOneResult(dartResult);
-		int score = Integer.parseInt(splitedOneResult[0]);		// 점수
-		String area = splitedOneResult[1];						// 영역
-		String opt = splitedOneResult[2]!=null ? splitedOneResult[2] : "";	// 옵션
-		
-		
-		// 영역 점수 계산
-		if(area.equals("D"))
-		{
-			score = (int) Math.pow(score, 2);
-		}
-		else if(area.equals("T"))
-		{
-			score = (int) Math.pow(score, 3);
-		}
-		
-		if(opt.equals("*"))
-		{
-			score *=2;
-			if(i>0)
-			{
-				answer[i-1] *= 2;
-			}
-		}
-		else if(opt.equals("#"))
-		{
-			score *= (-1);
-		}
-		
-		
-		return score;
-	}
 	
 	public static int getSum(int[] arr)
 	{
@@ -88,16 +18,44 @@ public class Solution {
 	}
 	
 	public static int solution(String dartResult) {
-		
-		
-		String[] splitedDartResult = splitDartResult(dartResult);
 		int[] answer = new int[3];
-		
-		for(int i=0;i<3;i++)
-		{	
-			answer[i] = cal(splitedDartResult[i], answer, i);
-		}
 	
+		Map<String, Integer> bonus = new HashMap<String, Integer>(){{
+			put("S",1);
+			put("D",2);
+			put("T",3);
+		}};
+		
+		Map<String, Integer> opt_map = new HashMap<String,Integer>(){{
+			put("*",2);
+			put("#",-1);
+			put("",1);
+		}};
+		
+		// "1D2S#10S" -> 1D 2S# 10S 
+		String regex = "(\\d+)([SDT])([\\*#]?)";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(dartResult);
+		
+		int i=0;
+		while(matcher.find())
+		{	
+			int score = Integer.parseInt(matcher.group(1));	// 점수
+			String area = matcher.group(2);	// 영역
+			String opt = matcher.group(3);	// 점수
+	
+			score = (int) Math.pow(score, bonus.get(area)) * opt_map.get(opt);
+			
+			
+			if(opt.equals("*") && i>0)
+			{
+				answer[i-1] *= 2;
+			}
+			
+			answer[i] = score;
+			i++;	
+		}
+		
 		return getSum(answer);
     }
 }
