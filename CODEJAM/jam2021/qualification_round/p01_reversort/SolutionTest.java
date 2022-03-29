b520ffa1df6746fa86e15a4aba945805b9fe633a
yonghwan <yonghwankim.dev@gmail.com>
2022-03-29 11:23:01 +0900

    codejam reversort

---
+++ CODEJAM/jam2021/qualification_round/p01_reversort/SolutionTest.java
@@ -0,0 +1,36 @@
+package CODEJAM.jam2021.qualification_round.p01_reversort;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+	@Test
+	void test() {
+		int n = 100;
+		List<Integer> L = new ArrayList<>();
+		
+		for(int i=1; i<=n; i++)
+		{
+			L.add(n-i+1);
+		}
+		System.out.println(Solution.reverSort(L));
+	}
+	
+	@Test
+	void test2() {
+		int n = 2;
+		List<Integer> L = new ArrayList<>();
+		
+		for(int i=1; i<=n; i++)
+		{
+			L.add(n-i+1);
+		}
+		System.out.println(Solution.reverSort(L));
+	}
+
+}
