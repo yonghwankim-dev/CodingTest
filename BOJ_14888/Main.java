a8259491c57e344c30613c4c926f0768f8985354
yonghwan <yonghwankim.dev@gmail.com>
2022-04-12 12:16:45 +0900

    boj 14888 solution

--- BOJ_14888/Main.java
+++ BOJ_14888/Main.java
@@ -4,42 +4,160 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 
 public class Main {
-	private static int cal(int[] arr, int[] op)
+	
+	static int N;
+	static boolean[] choosen;
+	static int max_val = Integer.MIN_VALUE;
+	static int min_val = Integer.MAX_VALUE;
+	
+	private static List<String> getOperationList(int[] op)
 	{
-		return 0;
+		List<String> op_list = new ArrayList<String>();
+		for(int i = 0; i < 4; i++)
+		{
+			while(op[i]!=0)
+			{
+				if(i==0)
+				{
+					op_list.add("+");
+				}
+				else if(i==1)
+				{
+					op_list.add("-");
+				}
+				else if(i==2)
+				{
+					op_list.add("*");
+				}
+				else
+				{
+					op_list.add("/");
+				}
+				op[i]--;
+			}
+		}
+		return op_list;
 	}
-	public static void solution(int N, int[] arr, int[] op)
+	
+	public static int cal(int a, int b, String op)
 	{
+		int result = 0;
+		switch(op)
+		{
+			case "+":
+					result = a + b;
+					break;
+			case "-":
+					result = a - b;
+					break;
+			case "*":
+					result = a * b;
+					break;
+			case "/":
+					result = a / b;
+					break;
+			default:
+					break;
+		}
+		return result;
+	}
+	
+	public static int makeExp(int[] arr, List<String> vc)
+	{
+		Stack<Integer> stack = new Stack<Integer>();
+		int vc_idx = 0;
+		int a,b, result=0;
+		int i=0;
+		String op;
 		
+		while(!stack.isEmpty() || i<arr.length)
+		{
+			if(stack.size()==2)
+			{
+				a = stack.pop();
+				b = stack.pop();
+				op = vc.get(vc_idx);
+				stack.add(cal(b, a, op));
+				vc_idx++;
+				
+				if(vc_idx==vc.size())
+				{
+					result = stack.pop();
+					break;
+				}
+			}
+			stack.add(arr[i]);
+			i++;
+		}
+		
+		return result;
 	}
+	
+	public static void backtracking(List<String> operator, List<String> cur_operator, int pos, int[] arr)
+	{
+		if(cur_operator.size()==N-1)
+		{	
+			int result = makeExp(arr, cur_operator);
+			max_val = Math.max(max_val, result);
+			min_val = Math.min(min_val, result);
+			return;
+		}
+		
+		for(int i=0; i<operator.size(); i++)
+		{
+			if(choosen[i])
+			{
+				continue;
+			}
+			
+			choosen[i] = true;
+			cur_operator.add(operator.get(i));
+			
+			backtracking(operator, cur_operator, pos+1, arr);
+			
+			choosen[i] = false;
+			cur_operator.remove(cur_operator.size()-1);
+		}
+	}
+	
+	public static void solution(int[] arr, int[] op)
+	{
+		List<String> operator = getOperationList(op);
+		List<String> cur_operator = new ArrayList<String>();
+		
+		choosen = new boolean[N-1];
+		backtracking(operator, cur_operator, 0, arr);	
+	}
+	
 	public static void main(String args[]) throws IOException
 	{
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		
-//		int N = Integer.parseInt(br.readLine());
-//		int[] arr = new int[N];
-//		int[] op = new int[4];
-//		
-//		// 피연산자 입력
-//		String[] str = br.readLine().split(" ");
-//		for(int i=0;i<arr.length;i++)
-//		{
-//			arr[i] = Integer.parseInt(str[i]);
-//		}
-//		
-//		str = br.readLine().split(" ");
-//		for(int i=0;i<4;i++)
-//		{
-//			op[i] = Integer.parseInt(str[i]);
-//		}
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		int[] op = new int[4];
 		
-		solution(6, new int[] {1,2,3,4,5,6}, new int[] {2,1,1,1});
+		// 피연산자 N개 입력
+		String[] str = br.readLine().split(" ");
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i] = Integer.parseInt(str[i]);
+		}
 		
+		// 연산자 입력 
+		str = br.readLine().split(" ");
+		for(int i=0;i<4;i++)
+		{
+			op[i] = Integer.parseInt(str[i]);
+		}
+		
+		solution(arr, op);
+		System.out.println(max_val);
+		System.out.println(min_val);
 	}
 }
